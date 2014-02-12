e55bfb132894ae8f84a6e45b955fcecb7b34edd7
dustan1 <goadustan@gmail.com>
2014-02-11 21:20:44 -0500

    testing branching

--- Painter.cpp
+++ Painter.cpp
@@ -21,14 +21,15 @@ Painter::~Painter()
       
 void Painter::screenWriter(int screenHeight, int screenWidth, char* screenData)//use of C string for class
 {
-        cout<<screenData<<endl;
-        cout<<"hello";
+   for (int j=1;j<screenHeight;j++){  
+     
+      for (int i=1;i<screenWidth;i++){
+           cout<<screenData[((j-1)*80)+i];
+      }    
+        //cout<<"hello";
+   }     
         
-        
-        
-        
-        
-        
+      
 }
    
 //void Painter::screenRedraw(screenGraphicData *screenData, int screenHeight, int screenWidth); 
