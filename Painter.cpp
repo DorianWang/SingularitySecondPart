677e82988194577d2bbb0cf4fbab56c5e782581a
dustan1 <goadustan@gmail.com>
2014-02-12 10:19:56 -0500

    added stuff

--- Painter.cpp
+++ Painter.cpp
@@ -17,10 +17,26 @@ Painter::~Painter()
                   
 }      
       
+
+bool Painter::screenWriter(int screenHeight, int screenWidth, char*screenData, int startX, int startY, int overwriteNum, HANDLE cHandle)
+{
+   COORD cursorStart;
+   if (startX<=screenWidth&&startX>0){
+   cursorStart.X=startX;   
+   }
+   
+   if (startY<=screenHeight&&startY>0){
+   cursorStart.Y=startY;   
+   }
+   SetConsoleCursorPosition(cHandle,cursorStart);
       
-      
+   return false;
+}      
+
 void Painter::screenWriter(int screenHeight, int screenWidth, char* screenData)//use of C string for class
 {
+     
+   for (int j=1;j<screenHeight;)
         cout<<screenData<<endl;
         cout<<"hello";
         
--- Painter.h
+++ Painter.h
@@ -2,6 +2,8 @@
 #ifndef PAINTER_H
 #define PAINTER_H 
 
+#include "windows.h"  
+
 #define SCREEN_HEIGHT 24
 #define SCREEN_WIDTH 80
 
@@ -10,19 +12,21 @@
 #include <iostream>
 
 
-
 using namespace std;
 
 class Painter
 {
+   
       
+    
+   HANDLE consoleHandle;   
    int stuff;
       
    public:
           
    Painter();
    ~Painter();
-   
+   bool screenWriter(int screenHeight, int screenWidth, char*screenData, int startX, int startY, int overwriteNum, HANDLE cHandle);
    void screenWriter(int screenHeight, int screenWidth, char* screenData);
    //void screenRedraw(tile screenTiles[24][80], int screenHeight, int screenWidth);
    
