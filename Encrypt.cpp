5526528fe98a848a56cc0558a207ed6e2654dbfc
dustan1 <goadustan@gmail.com>
2014-04-16 10:19:14 -0400

    asps

--- Encrypt.cpp
+++ Encrypt.cpp
@@ -6,23 +6,31 @@ int Encryter::keygen() //For creating new rotors, reflectors and things
    //I think I'll split them into 5 pieces. alphabet, upperAlphabet, numbers, important characters, and extras
    char alphabet[26];
    char upperAlphabet[26];
+   char importantChars[6]={'.', ',', '!', ' ', '?', 'a'};
    char numbers[10];
    // : = 58, " = 34, \ = 92, / = 47
-   char otherChars[27]={'!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ' ', '.', ',', ';', 'a', 'b', 'c', 'd', '~', '<', '>', '-', '_', '=', '+', '{', '}'};     
+   char otherChars[23]={'@', '#', '$', '%', '^', '&', '*', '(', ')', ';', 'a', 'b', 'c', 'd', '~', '<', '>', '-', '_', '=', '+', '{', '}'};     
+   
+   //Special chars that are annoying.
+   importantChars[5] = 34; 
+   otherChars[11] = 47; otherChars[12] = 58; otherChars[13] = 92;
+   
    //How many? 
-   //Chars I want: All ten shift+number, ".,;:/\|"'-_+=", " ", a newLine character which I'll have as "{" and a tab as "}", backspace as "~"
+   //newLine character which I'll have as "{" and a tab as "}", backspace as "~"
    
    //http://www.asciitable.com/
    
    int vectorCounter = 0;
-   
+
+if (){
    for (int i='a'-'a';i<'z'-'a'+1;i++){
       alphabet[i]='a' + i;
       cout<<alphabet[i];
       myvector.push_back(alphabet[i]);
       vectorCounter++;
    }
-cout<<endl;
+}
+//cout<<endl;
  
    for (int i='A'-'A';i<'Z'-'A' + 1;i++){
       upperAlphabet[i]='A' + i;
@@ -30,7 +38,7 @@ cout<<endl;
       myvector.push_back(upperAlphabet[i]);
       vectorCounter++;
    }
-cout<<endl;
+//cout<<endl;
 
 for (int i='0'-'0';i<'9'-'0' + 1;i++){
       numbers[i]='0' + i;
@@ -38,12 +46,12 @@ for (int i='0'-'0';i<'9'-'0' + 1;i++){
       myvector.push_back(numbers[i]);
       vectorCounter++;
    }
-cout<<endl;
-//char asdf = '!'+1;//"
+//cout<<endl;
 
-//15, 16, 17, 18
-//Special chars that are annoying.
-otherChars[14] = 58; otherChars[15] = 47; otherChars[16] = 34; otherChars[17] = 92;
+
+
+
+
 
 for (int i=0;i<25;i++){
 
