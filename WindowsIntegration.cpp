b5c22d341ab00324de3cfe63147a29fceb138ed8
dustan1 <goadustan@gmail.com>
2014-05-20 10:20:40 -0400

    cwad

--- WindowsIntegration.cpp
+++ WindowsIntegration.cpp
@@ -2,6 +2,7 @@
 #include "WindowsIntegration.h"
 
 //Finds all files in the directory
+//The user is required to append or prepend wildcards, etc.
 bool winCnrl::findAllFiles(char* fileName)
 {
 
@@ -9,7 +10,7 @@ if (directorySet == false){
    return false;//Directory is not set   
 }
      
-bool allFiles = false;
+//bool allFiles = false;
 int stringLength;
 std::string filePath; std::string temp;
 
@@ -20,45 +21,64 @@ HANDLE hFind = INVALID_HANDLE_VALUE;
 DWORD dwError=0;
 TCHAR szDir[MAX_FILE_PATH];
 
-if (fileName == NULL){
-   allFiles = true;
-}
-else
-{
-   stringLength = strlen(fileName);
-   
-   if ((stringLength + 3)>MAX_FILE_PATH){
-      return false;//String is too long...
+   if (fileName == NULL){
+      filePath = directoryPath + "\\*";//Search for all files and folders.
    }
+   else
+   {
+      stringLength = strlen(fileName);
+      
+      if ((stringLength + 3)>MAX_FILE_PATH){
+         return false;//String is too long...
+      }
+      filePath = directoryPath + fileName;
+   }    
+
+   hFind = FindFirstFile(filePath.c_str(), &tempFile);
+
+   if (INVALID_HANDLE_VALUE == hFind) 
+   {
+      return false;//Did not get anything...
+   } 
+   
+   do
+   {
+      allItemsInformation.push_back(tempFile);
+   } while (FindNextFile(hFind, &tempFile) != 0);
+    
+   dwError = GetLastError();
+   if (dwError != ERROR_NO_MORE_FILES){
+      return false;//Failure due to some problem...
+   }
+   
+   FindClose(hFind);
+   return true;
 }
 
-   
-   if (allFiles){
-      filePath = directoryPath + "\\*";//Search for all files and folders.
-      
-      
-   
-      hFind = FindFirstFile(szDir, &tempFile);
-   
-      if (INVALID_HANDLE_VALUE == hFind) 
-      {
-         return 0;//Did not get anything...
-      } 
-         do
-         {
-            allItemsInformation.push_back(tempFile);
-         }while (FindNextFile(hFind, &tempFile) != 0);
-       
-         dwError = GetLastError();
-         if (dwError != ERROR_NO_MORE_FILES){
-            return 0;//Failure due to some problem...
-         }
-      
-         FindClose(hFind);
-      
-   
-   }
+std::string getExecutablePath()
+{
+char pathName[MAX_FILE_PATH + 4];
+
+int charsGotten = GetModuleFileName(NULL, pathName, MAX_FILE_PATH + 4);
+
+pathName[charsGotten] = 0;
+std::string output = 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
