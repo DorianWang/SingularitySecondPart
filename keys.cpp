481cc34833a1ad8ee50578cdb462aacd35589431
dustan1 <goadustan@gmail.com>
2014-06-22 22:17:26 -0400

    small changes

--- keys.cpp
+++ keys.cpp
@@ -19,6 +19,7 @@ for(i=0;i<importantKeys.length();i++){
 
 //returns 1 for enter, 2 for escape,3 for TAB etc.
 //http://msdn.microsoft.com/en-us/library/windows/desktop/ms646293%28v=vs.85%29.aspx
+//Please do not use.
 int Keypress::get_code(){
    if (GetAsyncKeyState(VK_RETURN)){                                
       return 1;                                
@@ -42,7 +43,68 @@ int Keypress::get_code(){
     return -1;//No keys found
 }
 
-
+bool Keypress::get_code(int keyCode)
+{
+   switch (keyCode):
+          
+      case 1:
+         return GetAsyncKeyState(VK_RETURN);//Enter key
+         break;
+      case 2:
+         return GetAsyncKeyState(VK_ESCAPE);//Escape key
+         break;
+      case 3:
+         return GetAsyncKeyState(VK_TAB);//TAB key
+         break;
+      case 4:
+         return GetAsyncKeyState(VK_SPACE);//Spacebar
+         break;
+      case 5:
+         return GetAsyncKeyState(VK_BACK);//Backspace key
+         break;
+      case 6:    
+         return (GetAsyncKeyState(VK_LBUTTON)||GetAsyncKeyState(VK_RBUTTON))//Any mouse button
+      case 7:
+         
+      case 8:
+         
+      case 9:
+}
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 
