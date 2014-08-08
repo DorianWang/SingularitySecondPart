3e61e746b1180956601a82160cebe6552d81973d
dustan1 <goadustan@gmail.com>
2014-08-08 16:52:40 -0400

    random crap

--- stringFunctions.cpp
+++ stringFunctions.cpp
@@ -33,8 +33,45 @@ std::string stringFunc::popFirstToken(std::string input, const char* delim)
 std::vector <std::string> stringFunc::parseAllTokens(std::string input, const char* delim)
 {
    std::vector <std::string> tempVector;
-   //tempVector.push_back();
+   char tempCString[input.length()]; char* tempToken;
+   strcpy(tempCString, input.c_str());
+   tempToken = strtok(tempCString, delim);
+   
+   while(tempToken != NULL){
+      tempVector.push_back(tempToken);
+      tempToken = strtok(NULL, delim);
+   }
+   
+   return tempVector;
 }
+
+
+//delim is automatically added in between tokens
+std::string stringFunc::tokensToString(std::vector <std::string> input, const char* delim)
+{
+   std::string output;
+   
+   if (input.size() < 1){ return output; }//Returns an empty string.
+   
+   if (delim != NULL){
+      output = input[0];
+      for (int i=1; i<input.size(); i++){
+         output = output + delim + input[i];
+      }
+   }
+   else
+   {
+      for (int i=0; i<input.size(); i++){
+         output = output + input[i];
+      }
+   }
+   
+   
+   return output;
+}
+
+
+
 
 
 
